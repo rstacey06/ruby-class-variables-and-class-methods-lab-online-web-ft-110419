3a5e4bcfb29260275533c098cc203c19715df382
Burbage Stacey <rstacey06@gmail.com>
2019-11-13 14:54:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,6 +29,25 @@ class Song
     genre_count = {}
   end
   
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+  
+  
+  
+  
+  
+  
+  
+  
   def self.artist_count
     artist_count = {}
   end
